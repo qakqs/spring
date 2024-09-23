8f5ed354af953bf7486673684b7757ee68d8a2b5
12480 <1248001568@qq.com>
2024-09-23 19:06:17 +0800

    add

--- core-4/src/main/java/cn/mini/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ core-4/src/main/java/cn/mini/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -10,6 +10,7 @@ import cn.mini.beans.factory.support.Instantion.CglibSubclassingInstantiationStr
 import cn.mini.beans.factory.support.Instantion.InstantiationStrategy;
 
 import java.lang.reflect.Constructor;
+import java.util.Objects;
 
 public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
 
@@ -45,6 +46,9 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
     protected void applyPropertyValues(Object bean, String beanName, BeanDefinition beanDefinition) throws BeansException {
         try {
             PropertyValues propertyValues = beanDefinition.getPropertyValues();
+            if(Objects.isNull(propertyValues)) {
+                return;
+            }
             for (int i = 0; i < propertyValues.getPropertyValues().length; i++) {
                 String name = propertyValues.getPropertyValues()[i].getName();
                 Object value = propertyValues.getPropertyValues()[i].getValue();
