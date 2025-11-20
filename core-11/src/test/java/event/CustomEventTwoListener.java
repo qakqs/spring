79f617cbfb1026b917ebac186fddac7b734db7e5
12480 <1248001568@QQ.COM>
2025-11-20 18:40:30 +0800

    -12 begin

---
+++ core-11/src/test/java/event/CustomEventTwoListener.java
@@ -0,0 +1,14 @@
+package event;
+
+import cn.mini.context.ApplicationListener;
+
+import java.util.Date;
+
+public class CustomEventTwoListener implements ApplicationListener<CustomEvent> {
+    @Override
+    public void onApplicationEvent(CustomEvent event) {
+        System.out.println("2收到：" + event.getSource() + "消息;时间：" + new Date());
+        System.out.println("2消息：" + event.getId() + ":" + event.getMessage());
+
+    }
+}
